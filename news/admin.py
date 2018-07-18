007eedcb8add35ee63b65c6f1f6cb6d068e98657
maxwellharon <maxwellharon54@gmail.com>
2018-07-18 13:52:03 +0300

    Add migrations

--- news/admin.py
+++ news/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Editor,Post,tags
+from .models import Editor, Post, tags, Location
 
 # Register your models here.
+
+
 class PostAdmin(admin.ModelAdmin):
-    filter_horizontal =('tags',)
+    filter_horizontal = ('tags',)
 
 
 admin.site.register(Editor)
-admin.site.register(Post),PostAdmin
+admin.site.register(Post), PostAdmin
 admin.site.register(tags)
+admin.site.register(Location)

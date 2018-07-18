7b90ed876fa4d22a40dd48e968104acbb59791cb
maxwellharon <maxwellharon54@gmail.com>
2018-07-18 13:56:09 +0300

    Add the DjangoWhiteNoise(application)

--- tribune/wsgi.py
+++ tribune/wsgi.py
@@ -1,3 +1,11 @@
+import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tribune.settings")
+
+
+
 """
 WSGI config for tribune project.
 
@@ -7,10 +15,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tribune.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)

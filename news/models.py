3a090338e890f340296364e916be20ae602dace8
maxwellharon <maxwellharon54@gmail.com>
2018-07-18 13:53:32 +0300

    Removal of the Css file

--- news/models.py
+++ news/models.py
@@ -4,60 +4,63 @@ import datetime as dt
 # Create your models here.
 
 
-
-
-
 class Editor(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
     email = models.EmailField()
-    phone_number = models.CharField(max_length = 10,blank =True)
+    phone_number = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         return self.first_name
+
         class Meta:
             ordering = ['name']
-
 
     def save_editor(self):
         self.save()
 
 
-
 class tags(models.Model):
-    name = models.CharField(max_length = 30)
+    name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
 
 
+class Location(models.Model):
+    name = models.TextField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
-    title = models.CharField(max_length =60)
+    title = models.CharField(max_length=60)
     post = models.TextField()
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
+    location = models.ForeignKey(Location)
     pub_date = models.DateTimeField(auto_now_add=True)
-    post_image = models.ImageField(upload_to = 'posts/')
+    post_image = models.ImageField(upload_to='posts/')
 
     @classmethod
-    def days_news(cls,date):
-        news = cls.objects.filter(pub_date__date = date)
+    def days_news(cls, date):
+        news = cls.objects.filter(pub_date__date=date)
         return news
 
     @classmethod
     def todays_news(cls):
-       today = dt.date.today()
-       news = cls.objects.filter(pub_date__date = today)
-       return news
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date=today)
+        return news
 
     @classmethod
-    def search_by_title(cls,search_term):
+    def search_by_title(cls, search_term):
         news = cls.objects.filter(title__icontains=search_term)
         return news
-       # @classmethod
-       # def todays_news(cls):
-       #     today = dt.date.today()
-       #     news = cls.objects.filter(pub_date__date = today)
-       #     return news
+        # @classmethod
+        # def todays_news(cls):
+        #     today = dt.date.today()
+        #     news = cls.objects.filter(pub_date__date = today)
+        #     return news
 # class Article(models.Model):
